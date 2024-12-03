3fb81a22624755c8487518c31ffff2e39e3384b2
Alex Jablonski <git@ajablonski.dev>
2024-12-02 23:23:27 -0600

    Solve day 3 part 1

--- src/problem3/mod.rs
+++ src/problem3/mod.rs
@@ -1,8 +1,31 @@
+use regex::Regex;
 use crate::Problem;
 
 pub struct Problem3 {}
 
-impl Problem<u128> for Problem3 {}
+impl Problem<u128> for Problem3 {
+    fn part1(&self, input: &str) -> u128 {
+        let instruction_re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+        instruction_re
+            .captures_iter(input)
+            .map(|c| {
+                match c.extract() {
+                    (_, [l , r]) => {
+                        match (l.parse::<u32>(), r.parse::<u32>()) {
+                            (Ok(l), Ok(r)) => (l * r) as u128,
+                            _ => panic!("Unable to parse numbers {l:?}, {r:?}")
+                        }
+                    },
+                }
+            })
+            .sum()
+    }
+
+    fn part2(&self, input: &str) -> u128 {
+        0
+    }
+}
 
 pub const PROBLEM3: Problem3 = Problem3 {};
 
@@ -12,9 +35,8 @@ mod tests {
     const P: Problem3 = Problem3 {};
 
     #[test]
-    #[should_panic]
-    fn test_part1() {
-        assert_eq!(P.part1(""), 0);
+    fn should_solve_part_1_example() {
+        assert_eq!(P.part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"), 161);
     }
 
     #[test]
