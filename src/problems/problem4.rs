9ec79bf39e574c37417eb92f13cfec5329e463fa
Alex Jablonski <git@ajablonski.dev>
2024-12-04 13:22:39 -0600

    Problem 4: perform single pass through grid for part 1

--- src/problems/problem4.rs
+++ src/problems/problem4.rs
@@ -1,28 +1,64 @@
 use crate::problems::Problem;
 use crate::Event;
-use std::ops::Add;
+use std::fmt;
+use std::fmt::Write;
 use std::sync::mpsc::Sender;
 
 pub struct Problem4 {}
 
-impl Problem<u128> for Problem4 {
-    fn part1(&self, input: &str, tx: Sender<Event>) -> u128 {
-        let horizontal_count = input
+struct Grid {
+    lines: Vec<Vec<char>>,
+    row_count: usize,
+    col_count: usize,
+}
+
+impl fmt::Debug for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_char('\n')?;
+        self.lines.iter().for_each(|line| {
+            line.iter().for_each(|c| f.write_char(*c).unwrap());
+
+            f.write_char('\n').unwrap();
+        });
+
+        Ok(())
+    }
+}
+
+impl Grid {
+    fn from_lines(lines: Vec<Vec<char>>) -> Self {
+        Self {
+            row_count: lines.len(),
+            col_count: lines[0].len(),
+            lines,
+        }
+    }
+
+    fn from_string(input: &str) -> Self {
+        let lines = input
             .lines()
-            .map(|l| Self::find_xmas(&l.to_string()))
-            .sum::<usize>();
+            .map(|l| l.chars().collect::<Vec<char>>())
+            .collect();
 
-        let vertical_count = Self::get_vertical_lines(input, &tx)
-            .iter()
-            .map(Self::find_xmas)
-            .sum::<usize>();
+        Self::from_lines(lines)
+    }
+}
 
-        let diagonal_count = Self::get_diagonal_lines(input, &tx)
-            .iter()
-            .map(Self::find_xmas)
-            .sum::<usize>();
+impl Problem<u128> for Problem4 {
+    fn part1(&self, input: &str, _tx: Sender<Event>) -> u128 {
+        let grid = Grid::from_string(input);
 
-        horizontal_count as u128 + vertical_count as u128 + diagonal_count as u128
+        (0..grid.row_count)
+            .map(|i| {
+                (0..grid.col_count)
+                    .map(|j| {
+                        Self::check_horizontal(i, j, &grid)
+                            + Self::check_vertical(i, j, &grid)
+                            + Self::check_diagonal(i, j, &grid)
+                    })
+                    .sum::<u128>() as u128
+            })
+            .sum()
     }
 
     fn part2(&self, input: &str, _tx: Sender<Event>) -> u128 {
@@ -66,54 +102,87 @@ impl Problem<u128> for Problem4 {
 }
 
 impl Problem4 {
-    fn find_xmas(line: &String) -> usize {
-        line.matches("XMAS").count() + line.matches("SAMX").count()
+    fn check_horizontal(row: usize, column: usize, grid: &Grid) -> u128 {
+        let mut total = 0;
+        let word_as_chars = "XMAS".chars().collect::<Vec<char>>();
+        let word_length = word_as_chars.len();
+        if grid.lines[row][column] == word_as_chars[0] {
+            if column >= word_length - 1
+                && grid.lines[row][column - 1] == word_as_chars[1]
+                && grid.lines[row][column - 2] == word_as_chars[2]
+                && grid.lines[row][column - 3] == word_as_chars[3]
+            {
+                total += 1
+            }
+            if column + word_length - 1 < grid.col_count
+                && grid.lines[row][column + 1] == word_as_chars[1]
+                && grid.lines[row][column + 2] == word_as_chars[2]
+                && grid.lines[row][column + 3] == word_as_chars[3]
+            {
+                total += 1
+            }
+        }
+        total
     }
 
-    fn get_vertical_lines(input: &str, _tx: &Sender<Event>) -> Vec<String> {
-        let column_count = input.lines().next().unwrap().len();
-
-        let mut strs: Vec<String> = Vec::new();
-
-        for i in 0..column_count {
-            strs.push(
-                input
-                    .lines()
-                    .fold(String::new(), |result, l| result.add(&l[i..i + 1])),
-            );
+    fn check_diagonal(row: usize, column: usize, grid: &Grid) -> u128 {
+        let word_as_chars = "XMAS".chars().collect::<Vec<char>>();
+        let word_length = word_as_chars.len();
+        let mut total = 0;
+        if grid.lines[row][column] == word_as_chars[0]{
+            if row >= word_length - 1 && column + word_length - 1 < grid.col_count // /, upwards
+                && grid.lines[row - 1][column + 1] == word_as_chars[1]
+                && grid.lines[row - 2][column + 2] == word_as_chars[2]
+                && grid.lines[row - 3][column + 3] == word_as_chars[3]
+            {
+                total += 1
+            }
+            if row + word_length - 1 < grid.row_count && column + word_length - 1 < grid.col_count // \, downwards
+                && grid.lines[row + 1][column + 1] == word_as_chars[1]
+                && grid.lines[row + 2][column + 2] == word_as_chars[2]
+                && grid.lines[row + 3][column + 3] == word_as_chars[3]
+            {
+                total += 1
+            }
+            if row + word_length - 1 < grid.row_count && column >= word_length - 1 // /, downwards
+                && grid.lines[row + 1][column - 1] == word_as_chars[1]
+                && grid.lines[row + 2][column - 2] == word_as_chars[2]
+                && grid.lines[row + 3][column - 3] == word_as_chars[3]
+            {
+                total += 1
+            }
+            if row >= word_length - 1 && column >= word_length - 1 // \, upwards
+                && grid.lines[row - 1][column - 1] == word_as_chars[1]
+                && grid.lines[row - 2][column - 2] == word_as_chars[2]
+                && grid.lines[row - 3][column - 3] == word_as_chars[3]
+            {
+                total += 1
+            }
         }
 
-        strs
+        total
     }
 
-    fn get_diagonal_lines(input: &str, _tx: &Sender<Event>) -> Vec<String> {
-        let column_count = input.lines().next().unwrap().len();
+    fn check_vertical(row: usize, column: usize, grid: &Grid) -> u128 {
+        let word_as_chars = "XMAS".chars().collect::<Vec<char>>();
+        let word_length = word_as_chars.len();
+        let mut total = 0;
+        if grid.lines[row][column] == word_as_chars[0] {
+            if row >= word_length - 1
+                && grid.lines[row - 1][column] == word_as_chars[1]
+                && grid.lines[row - 2][column] == word_as_chars[2]
+                && grid.lines[row - 3][column] == word_as_chars[3] {
+                total += 1
+            }
 
-        let row_count = input.lines().count();
-
-        let mut negative_slope_strs: Vec<String> =
-            vec![String::new(); column_count + row_count - 1];
-
-        input.lines().enumerate().for_each(|(i, line)| {
-            line.char_indices().for_each(|(j, c)| {
-                let i1 = row_count + j - i - 1;
-                negative_slope_strs[i1].push(c)
-            });
-        });
-
-        let mut positive_slope_strs: Vec<String> =
-            vec![String::new(); column_count + row_count - 1];
-
-        input.lines().enumerate().for_each(|(i, line)| {
-            line.char_indices().for_each(|(j, c)| {
-                let i1 = i + j;
-                positive_slope_strs[i1].push(c)
-            });
-        });
-
-        negative_slope_strs.append(positive_slope_strs.as_mut());
-
-        negative_slope_strs
+            if row + 3 < grid.row_count
+                && grid.lines[row + 1][column] == word_as_chars[1]
+                && grid.lines[row + 2][column] == word_as_chars[2]
+                && grid.lines[row + 3][column] == word_as_chars[3] {
+                total += 1
+            }
+        }
+        total
     }
 }
 
